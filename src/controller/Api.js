0197c25d8dabcccda2ecbf7e1f83eb9b20947b14
David Mariano Gimenez Mota <67445845+DDMotinha@users.noreply.github.com>
2020-11-18 22:39:19 -0300

    Inserindo inicialmente a API

---
+++ src/controller/Api.js
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from  'react';
+
+function apiRest(){
+    const URL = "URL";
+    const TOKEN = "TOKEN";
+
+    const [get, setGet] = useState(Value(URL));
+    
+    function consumeApi(){
+        fetch(URL, {
+            method: "GET",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringfy(data),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Sucesso:', data);
+        })
+        .catch((error) => {
+            console.error('Erro:', error)
+        })
+    };
+}
+
+export default apiRest;
